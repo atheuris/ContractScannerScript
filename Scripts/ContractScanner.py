6463ad53a69d0abf487a1de1ebf16a9db38a2115
Robin <s1186143@student.windesheim.nl>
2023-09-05 11:20:05 +0200

    Api for gathering contract info on userwallet adress.

--- Scripts/ContractScanner.py
+++ Scripts/ContractScanner.py
@@ -6,8 +6,32 @@ app = Flask(__name__)
 ETHEREUM_SCAN_API = "https://api.etherscan.io/api"
 API_KEY = "7UI59WUIDUBUDJRQDNUG45WQ5CY4R8SWX2"
 
+def check_liquidity_pool_creation(contract_address):
+    # ... existing code ...
+
+    # Example code to check for specific logs
+    payload = {
+        "module": "logs",
+        "action": "getLogs",
+        "fromBlock": "0",
+        "toBlock": "latest",
+        "address": contract_address,
+        # Include specific topic for the event or method indicative of liquidity pool creation
+        "topic0": "TOPIC_SIGNATURE_HERE",
+        "apikey": API_KEY
+    }
+
+    response = requests.get(ETHEREUM_SCAN_API, params=payload)
+    if response.status_code == 200:
+        logs = response.json().get('result', [])
+        if logs:
+            return "Liquidity Pool"
+
+    return "Unknown"
+
+
 def get_contracts_by_wallet(wallet_address):
-    contract_addresses = []
+    contract_details = []
 
     # API params
     payload = {
@@ -27,11 +51,15 @@ def get_contracts_by_wallet(wallet_address):
         transactions = response.json().get('result', [])
         
         for tx in transactions:
-            # Check if the transaction is a contract creation
             if tx['to'] == '':
-                contract_addresses.append(tx['contractAddress'])
+                contract_address = tx['contractAddress']
+                liquidity_status = check_liquidity_pool_creation(contract_address)
+                contract_details.append({
+                    'contractAddress': contract_address,
+                    'liquidityStatus': liquidity_status
+                })
                 
-    return contract_addresses
+    return contract_details
 
 @app.route('/get_contracts', methods=['POST'])
 def get_contracts():
@@ -39,8 +67,8 @@ def get_contracts():
     if not wallet_address:
         return jsonify({'error': 'Missing wallet_address parameter'}), 400
 
-    contract_addresses = get_contracts_by_wallet(wallet_address)
-    return jsonify({'contract_addresses': contract_addresses}) 
+    contract_details = get_contracts_by_wallet(wallet_address)
+    return jsonify({'contracts': contract_details}) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
